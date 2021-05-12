b7bcd0490b8b58b57a78bcbc1da0b9882b6d9a87
Oluwatosin-Bakare <oluwatosin.bakare@yahoo.com>
2021-05-12 18:14:20 +0100

    Fixed Routing Bug

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,31 +1,39 @@
 const puppeteer = require('puppeteer');
 
-exports.getPage =  async (req, res) => {
-    const siteName = req.query.url
-    console.log(siteName);
-    const doSomething = async () => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto(siteName.toString(), {waitUntil: 'networkIdle2'});
-        let price = await page.evaluate(() => {         
-            const details = {
-                image: document.querySelector('.-fh').src,
-                description: document.querySelector('.-fs20').textContent,
-                rating: {
-                    stars: document.querySelector('.stars').textContent,
-                    numbers: document.querySelector('.-plxs').textContent,
-                },
-                price: {
-                    current_price: document.querySelector('.-fs24').textContent,
-                    old_price: document.querySelector('.-lthr').textContent,
-                    discount: document.querySelector('._dsct').textContent
-                },
-            }
-            return details;       
-        })
-        await browser.close();
-        return price;
-    };
-    const response = await doSomething();
-    res.json(response)
+exports.getpage =  async (req, res) => {
+    try {
+        const sitename = req.query.url
+        console.log(sitename);
+        const doSomething = async () => {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto(sitename.toString(), {waitUntil: 'networkidle2'});
+            let scrapedDetails = await page.evaluate(() => {         
+                const details = {
+                    image: document.querySelector('.-fh').src,
+                    description: document.querySelector('.-fs20').textContent,
+                    rating: {
+                        stars: document.querySelector('.stars').textContent,
+                        numbers: document.querySelector('.-plxs').textContent,
+                    },
+                    price: {
+                        current_price: document.querySelector('.-fs24').textContent,
+                        old_price: document.querySelector('.-lthr').textContent,
+                        discount: document.querySelector('._dsct').textContent
+                    },
+                }
+                return details;       
+            })
+            await browser.close();
+            return scrapedDetails;
+        };
+
+        const response = await doSomething();
+        res.json(response)
+    }
+    catch(ex) {
+        res.status(500).json(`Oops, Something Went Wrong`);
+        console.log(`${ex} Oops, Something Went Wrong`);
+    }
+
 }
